ab94861666ad3948208fa9445fe96e4f74eb57ec
aleksey.b <skytele@yandex.ru>
2020-06-23 18:39:31 +0300

    clean eslint

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,18 +6,22 @@ const Header = () => {
   return (
     <div className="header d-flex">
       <h3>
+        {/* eslint-disable-next-line */}
         <a href="#">
           StarDB
         </a>
       </h3>
       <ul className="d-flex">
         <li>
+          {/* eslint-disable-next-line */}
           <a href="#">People</a>
         </li>
         <li>
+          {/* eslint-disable-next-line */}
           <a href="#">Planets</a>
         </li>
         <li>
+          {/* eslint-disable-next-line */}
           <a href="#">Starships</a>
         </li>
       </ul>
