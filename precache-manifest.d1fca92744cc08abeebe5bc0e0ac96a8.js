3939886c193f97bd3e40954d1acdd5024286e219
aleksey.b <skytele@yandex.ru>
2020-06-23 22:25:08 +0300

    Updates

---
+++ precache-manifest.d1fca92744cc08abeebe5bc0e0ac96a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2786b04ab537878f9b12149d57de3d39",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "ffe5ad0bd03cc4f28d64",
+    "url": "/star-db/static/css/main.4cf78946.chunk.css"
+  },
+  {
+    "revision": "97b7cdad34fa3fa13250",
+    "url": "/star-db/static/js/2.16ea75ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-db/static/js/2.16ea75ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe5ad0bd03cc4f28d64",
+    "url": "/star-db/static/js/main.ed996efa.chunk.js"
+  },
+  {
+    "revision": "dd843139110526c0b457",
+    "url": "/star-db/static/js/runtime-main.5b4e24ef.js"
+  }
+]);
