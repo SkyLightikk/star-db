8a8335d797928755fe36e9c8d35253502a89816a
aleksey.b <skytele@yandex.ru>
2020-07-21 15:52:25 +0300

    use react router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service'
-import { SwapiServiceProvider } from '../swapi-service-context';
-import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import {SwapiServiceProvider} from '../swapi-service-context';
+import {PeoplePage, PlanetsPage, StarshipsPage} from '../pages';
 
 import './app.css';
+
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 export default class App extends Component {
 
@@ -17,9 +19,9 @@ export default class App extends Component {
   };
 
   onServiceChange = () => {
-    this.setState(({ swapiService }) => {
-      const Service = swapiService instanceof SwapiService ? 
-                      DummySwapiService : SwapiService;
+    this.setState(({swapiService}) => {
+      const Service = swapiService instanceof SwapiService ?
+        DummySwapiService : SwapiService;
       return {
         swapiService: new Service()
       };
@@ -30,15 +32,16 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService}>
-        <div className="stardb-app">
-          <Header onServiceChange={this.onServiceChange} />
-          <RandomPlanet/>
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange}/>
+              <RandomPlanet/>
+              <Route path="/people" component={PeoplePage}/>
+              <Route path="/planets" component={PlanetsPage}/>
+              <Route path="/starships" component={StarshipsPage}/>
 
-          <PeoplePage/>
-          <PlanetsPage/>
-          <StarshipsPage/>
-
-        </div>
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
